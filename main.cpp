743da7c076ca525cce60b7793a6a4f2189f784d3
vuheeer <vuheeer@users.noreply.github.com>
2018-02-06 15:09:03 -0400

    Added client file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//main.cpp
+#include "Class.H"
+#include <iostream>
+#include <vector>
+using namepace std;
+
+int main() {
+	Class mow();
+	Class mow2= {3, 'c'};
+	Class mow3= {10, 'a'};
+	vector <Class> z;
+	v.push_back (mow);
+	v.push_back (mow3);
+	v.push_back (mow2);
+	for (auto x :: z) {
+		cout << x.GetYear() << x.GetStream() << endl;
+	}
+return 0;
+}
