e29afec02d7866cfc32194653f14c4653a4eb961
vuheeer <vuheeer@users.noreply.github.com>
2018-02-06 15:06:37 -0400

    Added header file

---
+++ Class.h
@@ -0,0 +1,18 @@
+//headerfile
+//Class.h
+#ifndef CLASS_H
+#define CLASS_H
+
+class Class {
+	public:
+		Class();
+		Class(int, char);
+		void SetYear(int);
+		int GetYear();
+		void SetStream(char);
+		char GetStream();
+	private:
+		int year
+		char stream
+}
+#endif
